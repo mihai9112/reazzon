c1c5ebf9b7c5173bc1b2af7710665d825d5fc3b0
Mihai Neagoe <nm9112@gmail.com>
2018-10-23 12:47:23 +0100

    Testing webhook

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Reazzon Home Page'),
+      home: new MyHomePage(title: 'Reazzon'),
     );
   }
 }
