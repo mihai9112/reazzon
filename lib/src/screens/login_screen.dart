0a14fe8bcd79f0978a92289923fd23f63b292ad5
Mihai Neagoe <nm9112@gmail.com>
2018-11-07 21:08:02 +0000

    Adds login scaffold

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context){
+    final bloc = Provider.of(context);
+
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(bloc),
+          passwordField(bloc),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType:  TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you:example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          decoration: InputDecoration(
+            hintText: 'password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  dynamic submitButton() { //dynamic here instead of widget due to weird error
+    return RaisedButton(
+      child: Text('Login'), 
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
