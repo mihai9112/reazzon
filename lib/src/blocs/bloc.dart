a5ee59b011ac80416b444ce5b36de06c6f712be7
Mihai Neagoe <nm9112@gmail.com>
2018-11-09 15:35:38 +0000

    Submit button

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,17 +1,25 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc with Validators {
-  final _email = StreamController<String>();
-  final _password = StreamController<String>();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
 
   // Add data to stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+
+  Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e, p) => true);
   
   // Change data
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  submit() {
+    final validEmail = _email.value;
+    final validPassword = _password.value;
+  }
 
   void dispose() {
     _email.close();
