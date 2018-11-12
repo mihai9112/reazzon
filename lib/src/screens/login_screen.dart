5de0f3583aec94ff01ce72c7effe0422a097fe29
Mihai Neagoe <nm9112@gmail.com>
2018-11-12 17:42:43 +0000

    Adds 3rd party logins

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
 import '../blocs/provider.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
+  static const _kFontFam = 'reazzon';
   final _pageController = new PageController(initialPage: 0, viewportFraction: 1.0);
+  static const IconData facebookIcon = const IconData(0xe801, fontFamily: _kFontFam);
+  static const IconData googleIcon = const IconData(0xf1a0, fontFamily: _kFontFam);
 
   Widget build(context){
     final bloc = Provider.of(context);
@@ -263,6 +267,207 @@ class LoginScreen extends StatelessWidget {
               ],
             ),
           ),
+          Container(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 30.0),
+                child: FlatButton(
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                      fontSize: 15.0,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                  onPressed: () => {}, //TODO:Functionality to be added
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0),
+            alignment: Alignment.center,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    color: Colors.blueAccent,
+                    onPressed: () => {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20.0,
+                        horizontal: 20.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              "LOGIN",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0),
+            alignment: Alignment.center,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(border: Border.all(width: 0.25)),
+                  ),
+                ),
+                Text(
+                  "OR CONNECT WITH",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(border: Border.all(width: 0.25)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(right: 8.0),
+                    alignment: Alignment.center,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: FlatButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            color: Color(0Xff3B5998),
+                            onPressed: () => {}, //TODO: Add functionality
+                            child: Container(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: FlatButton(
+                                      onPressed: ()=>{},
+                                      padding: EdgeInsets.only(
+                                        top: 20.0,
+                                        bottom: 20.0,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: <Widget>[
+                                          Icon(
+                                            facebookIcon,
+                                            color: Colors.white,
+                                            size: 15.0,
+                                          ),
+                                          Text(
+                                            "FACEBOOK",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 8.0),
+                    alignment: Alignment.center,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: FlatButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            color: Color(0Xffdb3236),
+                            onPressed: () => {}, //TODO:Add functionality
+                            child: Container(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: FlatButton(
+                                      onPressed: ()=>{},
+                                      padding: EdgeInsets.only(
+                                        top: 20.0,
+                                        bottom: 20.0,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: <Widget>[
+                                          Icon(
+                                            googleIcon,
+                                            color: Colors.white,
+                                            size: 15.0,
+                                          ),
+                                          Text(
+                                            "GOOGLE",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
